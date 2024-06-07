6bd1561196fbb9ddbcadc99fde0637b234471a37
ArtemSadovenko <asadovenko7@gmail.com>
2024-06-07 15:38:43 +0300

    Add: stats

--- src/pages/AssistantPage.tsx
+++ src/pages/AssistantPage.tsx
@@ -1,21 +1,45 @@
 import React from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import Button from "@mui/material/Button";
 import SidePanel from "../components/SidePanel";
 import SearchPanel from "../components/SearchPanel";
 import TopBar from "../components/TopBar";
 import TextField from "@mui/material/TextField";
 import { Height } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
+
 
 function AssistantPage() {
+  const navigator = useNavigate();
+
+
+
   return (
     <Container maxWidth="lg" style={{ padding: 0 }}>
       <Grid container spacing={3} sx={{ padding: 0 }}>
         <SidePanel />
 
         <Grid item md={9}>
+          
           <TopBar />
-
+          <div style={{margin: "10px 20px 10px 20px "}}>
+            <Button sx={{
+              color: "black",
+              borderBottom: "solid black 1px",
+              margin: "8px 16px 8px 16px "
+            }} onClick={() =>{
+                navigator("/dashboard/ai-assistant")
+            }}>Ai Asistant</Button>
+            <Button
+            sx={{
+              color: "black",
+              borderBottom: "solid black 1px",
+              margin: "8px 16px 8px 16px "
+            }}  onClick={() =>{
+                navigator("/dashboard/statistics")
+            }}>Statistics</Button>
+         </div>
           <h2>Get ideas how to improve your letter with A.I.</h2>
 
           {/* <TextField
